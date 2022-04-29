7e0aa8844c5bb8284300caadbd4185c21e19171a
Hwete <104570913+Hwete@users.noreply.github.com>
2022-04-29 08:06:52 -0400

    Create github_test.py

---
+++ github_test.py
@@ -0,0 +1,7 @@
+fav_team = input("What is your favorite nba team!")
+
+if fav_team == "Celtics":
+    print("Celtics in 4")
+    print("Next is Celtics take Warriors in Nba championships and win in a 7 game series")
+else:
+    print("Why not you live in Massachusetts?")
